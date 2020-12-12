58699de00ebb7c1d4ec4386ca97d279ec45ff725
kmihara <mihara@eaglys.co.jp>
2020-12-12 12:24:58 +0900

    [UPD] batch complete

--- lstm_livedoor/data.py
+++ lstm_livedoor/data.py
@@ -13,6 +13,7 @@ import matplotlib.pyplot as plt
 import torch
 import torch.nn as nn
 from sklearn.model_selection import train_test_split
+from sklearn.utils import shuffle
 import torch.optim as optim
 
 from model import LSTMClassifier
@@ -63,6 +64,19 @@ def category2tensor(cat, category2index):
     return torch.tensor([category2index[cat]], dtype=torch.long)
 
 
+# function to make batch
+def train2batch(title, category, batch_size=100):
+    title_batch = []
+    category_batch = []
+    title_shuffle, category_shuffle = shuffle(title, category)
+    for i in range(0, len(title), batch_size):
+        title_batch.append(title_shuffle[i:i+batch_size])
+        category_batch.append(category_shuffle[i:i+batch_size])
+    return title_batch, category_batch
+
+
+
+
 
 if __name__ == "__main__":
 
@@ -71,6 +85,8 @@ if __name__ == "__main__":
     datasets = get_title()
 
     word2index = {}
+    word2index.update({"<pad>":0})
+
     for title in datasets["title"]:
         wakati = make_wakati(title)
         for word in wakati:
@@ -83,10 +99,10 @@ if __name__ == "__main__":
     print(sentence2index(test, word2index))
 
 
+    embedding_dim = 200
+    hidden_dim = 128
     vocab_size = len(word2index)
 
-    embedding_dim = 10
-    hidden_dim = 6
 
     inputs = sentence2index(test, word2index)
 
@@ -115,124 +131,202 @@ if __name__ == "__main__":
 
     print(category2tensor("it-life-hack", category2index))
 
+    index_datasets_title_tmp = []
+    index_datasets_category = []
+
+    # pad to adjust the length to the longest one
+    max_len = 0
+    for title, category in zip(datasets["title"], datasets["category"]):
+        index_title = sentence2index(title, word2index)
+        index_category = category2tensor(category, category2index)
+        index_datasets_title_tmp.append(index_title)
+        index_datasets_category.append(index_category)
+        if max_len < len(index_title):
+            max_len = len(index_title)
+
+    # if the length is shorter than max_len, pad in front
+    index_datasets_title = []
+    for title in index_datasets_title_tmp:
+        for i in range(max_len - len(title)):
+            #print("****")
+            #print(type(title))
+            #print(title.shape)
+            title = torch.cat([torch.FloatTensor([0]), title])
+            #title.insert(0, 0)
+        index_datasets_title.append(title)
+
+    train_x, test_x, train_y, test_y = train_test_split(index_datasets_title, index_datasets_category, train_size=0.7)
+
+
+
 
     traindata, testdata = train_test_split(datasets, train_size=0.7)
 
-    embedding_dim = 10
-    hidden_dim = 12
-    vocab_size = len(word2index)
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
     tag_size = len(categories)
-    epochs = 5 
-
-    model = LSTMClassifier(embedding_dim, hidden_dim, vocab_size, tag_size)
-
+    model = LSTMClassifier(embedding_dim, hidden_dim, vocab_size, tag_size).to(device)
+    epochs = 5
+    optimizer = optim.Adam(model.parameters(), lr = 0.01)
     loss_function = nn.NLLLoss()
 
-    optimizer = optim.SGD(model.parameters(), lr = 0.01)
-
     losses = []
-
     for epoch in range(epochs):
         all_loss = 0
-        for i, (title, cat) in enumerate(zip(traindata["title"], traindata["category"])):
+        title_batch, category_batch = train2batch(train_x, train_y)
+        for i in range(len(title_batch)):
+            batch_loss = 0
+
             model.zero_grad()
-            inputs = sentence2index(title, word2index)
 
-            out = model(inputs)
+            # pass device=GPU to inference the tensor with GPU
+            title_tensor = torch.zeros(len(title_batch[i]), title_batch[i][0].size()[0]).long()
+            for j in range(len(title_tensor)):
+                title_tensor[j, :] = title_batch[i][j]
 
-            answer = category2tensor(cat, category2index)
+            #title_tensor = torch.tensor(title_batch[i], device=device)
 
-            loss = loss_function(out, answer)
+            # category_tensor.size() = (batch_size * 1) , so squeeze()
+            category_tensor = torch.tensor(category_batch[i], device=device).squeeze()
 
-            loss.backward()
+            out = model(title_tensor)
+
+            batch_loss = loss_function(out, category_tensor)
+            batch_loss.backward()
 
             optimizer.step()
 
-            all_loss += loss.item()
+            all_loss += batch_loss.item()
 
-        losses.append(all_loss)
         print("epoch", epoch, "\t", "loss", all_loss)
 
+        if all_loss < 0.1: break
     print("done.")
 
-    plt.plot(losses)
 
-
-    # validataion with testdata
-    test_num = len(testdata)
+    test_num = len(test_x)
 
     a = 0
 
     with torch.no_grad():
-        for i, (title, category) in enumerate(zip(testdata["title"], testdata["category"])):
+        title_batch, category_batch = train2batch(test_x, test_y)
 
-            inputs = sentence2index(title, word2index)
-            out = model(inputs)
+        for i in range(len(title_batch)):
+            title_tensor = torch.zeros(len(title_batch[i]), title_batch[i][0].size()[0]).long()
+            for j in range(len(title_batch[i])):
+                title_tensor[j, :] = title_batch[i][j]
+            #title_tensor = torch.tensor(title_batch[i], device=device)
+            category_tensor = torch.tensor(category_batch[i], device=device)
 
-            _, predict = torch.max(out, 1)
+            out = model(title_tensor)
 
-            answer = category2tensor(category, category2index)
-
-            if predict == answer:
-                a += 1
-
-    print("predict: {:.3f}".format(a/test_num))
-
-
-    # validataion with train data (to check if the model is over training)
-    traindata_num = len(traindata)
-    a = 0
-    with torch.no_grad():
-        for i, (title, category) in enumerate(zip(traindata["title"], traindata["category"])):
-            inputs = sentence2index(title, word2index)
-            out = model(inputs)
-
-            _, predict = torch.max(out, 1)
-            answer = category2tensor(category, category2index)
-            if predict == answer:
-                a += 1
-
-    print("predict : {:.3f}".format( a/traindata_num))
-
-
-    index2category = {}
-
-    for cat, idx in category2index.items():
-        index2category[idx] = cat
-
-    # answer -> correct label, predict -> lstm prediction, exact -> 0 if correct, X if incorrect
-    predict_df = pd.DataFrame(columns=["answer", "predict", "exact"])
-
-    with torch.no_grad():
-        for title, category in zip(testdata["title"], testdata["category"]):
-            out = model(sentence2index(title, word2index))
-            _, predict = torch.max(out, 1)
-            answer = category2tensor(category, category2index)
-            exact = "O" if predict.item() == answer.item() else "X"
-            s = pd.Series([answer.item(), predict.item(), exact], index=predict_df.columns)
-            predict_df = predict_df.append(s, ignore_index=True)
-
-
-    fscore_df = pd.DataFrame(columns=["category", "all", "precision", "recall", "fscore"])
-
-    prediction_count = collections.Counter(predict_df["predict"])
-
-    answer_count = collections.Counter(predict_df["answer"])
-
-    for i in range(len(category2index)):
-        all_count = answer_count[i]
-        precision = len(predict_df.query('predict == ' + str(i) + ' and exact == "O"'))/prediction_count[i]
-        recall = len(predict_df.query('answer == ' + str(i) + ' and exact == "O"')) / all_count
-        fscore = 2 * precision * recall / (precision + recall)
-        s = pd.Series([index2category[i], all_count, round(precision, 2), round(recall, 2), round(fscore, 2)], index=fscore_df.columns)
-        fscore_df = fscore_df.append(s, ignore_index=True)
-
-    print(fscore_df)
+            _, predicts = torch.max(out, 1)
+            for j, ans in enumerate(category_tensor):
+                if predicts[j].item() == ans.item():
+                    a += 1
+    print("predict : ", a / test_num)
 
 
 
 
 
-
+#    losses = []
+#
+#    for epoch in range(epochs):
+#        all_loss = 0
+#        for i, (title, cat) in enumerate(zip(traindata["title"], traindata["category"])):
+#            model.zero_grad()
+#            inputs = sentence2index(title, word2index)
+#
+#            out = model(inputs)
+#
+#            answer = category2tensor(cat, category2index)
+#
+#            loss = loss_function(out, answer)
+#
+#            loss.backward()
+#
+#            optimizer.step()
+#
+#            all_loss += loss.item()
+#
+#        losses.append(all_loss)
+#        print("epoch", epoch, "\t", "loss", all_loss)
+#
+#    print("done.")
+#
+#    plt.plot(losses)
+#
+#
+#    # validataion with testdata
+#    test_num = len(testdata)
+#
+#    a = 0
+#
+#    with torch.no_grad():
+#        for i, (title, category) in enumerate(zip(testdata["title"], testdata["category"])):
+#
+#            inputs = sentence2index(title, word2index)
+#            out = model(inputs)
+#
+#            _, predict = torch.max(out, 1)
+#
+#            answer = category2tensor(category, category2index)
+#
+#            if predict == answer:
+#                a += 1
+#
+#    print("predict: {:.3f}".format(a/test_num))
+#
+#
+#    # validataion with train data (to check if the model is over training)
+#    traindata_num = len(traindata)
+#    a = 0
+#    with torch.no_grad():
+#        for i, (title, category) in enumerate(zip(traindata["title"], traindata["category"])):
+#            inputs = sentence2index(title, word2index)
+#            out = model(inputs)
+#
+#            _, predict = torch.max(out, 1)
+#            answer = category2tensor(category, category2index)
+#            if predict == answer:
+#                a += 1
+#
+#    print("predict : {:.3f}".format( a/traindata_num))
+#
+#
+#    index2category = {}
+#
+#    for cat, idx in category2index.items():
+#        index2category[idx] = cat
+#
+#    # answer -> correct label, predict -> lstm prediction, exact -> 0 if correct, X if incorrect
+#    predict_df = pd.DataFrame(columns=["answer", "predict", "exact"])
+#
+#    with torch.no_grad():
+#        for title, category in zip(testdata["title"], testdata["category"]):
+#            out = model(sentence2index(title, word2index))
+#            _, predict = torch.max(out, 1)
+#            answer = category2tensor(category, category2index)
+#            exact = "O" if predict.item() == answer.item() else "X"
+#            s = pd.Series([answer.item(), predict.item(), exact], index=predict_df.columns)
+#            predict_df = predict_df.append(s, ignore_index=True)
+#
+#
+#    fscore_df = pd.DataFrame(columns=["category", "all", "precision", "recall", "fscore"])
+#
+#    prediction_count = collections.Counter(predict_df["predict"])
+#
+#    answer_count = collections.Counter(predict_df["answer"])
+#
+#    for i in range(len(category2index)):
+#        all_count = answer_count[i]
+#        precision = len(predict_df.query('predict == ' + str(i) + ' and exact == "O"'))/prediction_count[i]
+#        recall = len(predict_df.query('answer == ' + str(i) + ' and exact == "O"')) / all_count
+#        fscore = 2 * precision * recall / (precision + recall)
+#        s = pd.Series([index2category[i], all_count, round(precision, 2), round(recall, 2), round(fscore, 2)], index=fscore_df.columns)
+#        fscore_df = fscore_df.append(s, ignore_index=True)
+#
+#    print(fscore_df)
 
 
--- lstm_livedoor/model.py
+++ lstm_livedoor/model.py
@@ -1,6 +1,10 @@
 import torch
 import torch.nn as nn
 import torch.optim as optim
+import torch.nn.functional as F
+
+
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
 
 class LSTMClassifier(nn.Module):
@@ -11,23 +15,34 @@ class LSTMClassifier(nn.Module):
         self.hidden_dim = hidden_dim
 
         # dimension of word embeddings
-        self.word_embeddings = nn.Embedding(vocab_size, embedding_dim)
+        # padding_idx = 0 since <pad> has word id as 0
+        self.word_embeddings = nn.Embedding(vocab_size, embedding_dim, padding_idx=0)
 
         # nn.LSTM(embedding_dim, hidden_dim)
-        self.lstm = nn.LSTM(embedding_dim, hidden_dim)
+        # batch_first = True
+        self.lstm = nn.LSTM(embedding_dim, hidden_dim, batch_first=True)
 
         # hidden dimension to target_size dimension mapping
         self.hidden2tag = nn.Linear(hidden_dim, target_size)
 
-        self.softmax = nn.LogSoftmax(dim=1)
+        self.softmax = nn.LogSoftmax()
 
 
     def forward(self, sentence):
         embeds = self.word_embeddings(sentence)
-        embeds = embeds.view(embeds.size()[0], 1, embeds.size()[1])
+
+        # embeds.size() = (batch_size * len(sentence) * embedding_dim)
+        #embeds = embeds.view(embeds.size()[0], 1, embeds.size()[1])
         _, (lstm_out_h, lstm_out_c) = self.lstm(embeds)
-        tag_space = self.hidden2tag(lstm_out_h.view(-1, self.hidden_dim))
-        tag_scores = self.softmax(tag_space)
+        #tag_space = self.hidden2tag(lstm_out_h.view(-1, self.hidden_dim))
+
+        # lstm_out_h.size() = (1 * batch_size, hidden_dim)
+        tag_space  = self.hidden2tag(lstm_out_h)
+
+        # tag_space.size() = (1 * batch_size, tagset_size)
+        # to make (batch_size * tagset_size), do squeeze()
+        tag_scores = self.softmax(tag_space.squeeze())
+        #tag_scores = self.softmax(tag_space)
         return tag_scores
 
 
